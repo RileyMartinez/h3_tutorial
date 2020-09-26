4c806b90b3b100121e133e1b1b6ac119b691e580
Riley Martinez <riley.m.martinez@gmail.com>
2020-09-25 20:40:18 -0500

    Commit of second fail test

--- ISBNValidator/test/com/virtualpairprogrammers/isbntools/ValidateISBNTest.java
+++ ISBNValidator/test/com/virtualpairprogrammers/isbntools/ValidateISBNTest.java
@@ -7,8 +7,10 @@ import org.junit.Test;
 public class ValidateISBNTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void check_a_valid_ISBN() {
+		ValidateISBN validator = new ValidateISBN();
+		boolean result = validator.checkISBN(140449116);
+		assertTrue(result);
 	}
 
 }
