39807bb16b7df20e7236bf0982a2e4f8247f2582
Riley Martinez <riley.m.martinez@gmail.com>
2020-09-30 00:43:53 -0500

    Rename unit tests

--- ISBNValidator/test/com/virtualpairprogrammers/isbntools/ValidateISBNTest.java
+++ ISBNValidator/test/com/virtualpairprogrammers/isbntools/ValidateISBNTest.java
@@ -7,7 +7,7 @@ import org.junit.Test;
 public class ValidateISBNTest {
 
 	@Test
-	public void check_a_valid_ISBN() {
+	public void check_a_valid_10_digit_ISBN() {
 		ValidateISBN validator = new ValidateISBN();
 		boolean result = validator.checkISBN("0140449116");
 		assertTrue("first value", result);
@@ -25,27 +25,27 @@ public class ValidateISBNTest {
 	}
 	
 	@Test
-	public void isbn_numbers_ending_in_an_x_are_valid() {
+	public void tesn_digit_ISBN_numbers_ending_in_an_X_are_valid() {
 		ValidateISBN validator = new ValidateISBN();
 		boolean result = validator.checkISBN("012000030X");
 		assertTrue(result);
 	}
 	
 	@Test
-	public void check_an_invalid_ISBN() {
+	public void check_an_invalid_10_digit_ISBN() {
 		ValidateISBN validator = new ValidateISBN();
 		boolean result = validator.checkISBN("0140449117");
 		assertFalse(result);
 	}
 	
 	@Test(expected = NumberFormatException.class)
-	public void nine_digit_isbns_are_not_allowed() {
+	public void nine_digit_ISBNs_are_not_allowed() {
 		ValidateISBN validator = new ValidateISBN();
 		validator.checkISBN("123456789");
 	}
 	
 	@Test(expected = NumberFormatException.class)
-	public void non_digit_isbns_are_not_allowed() {
+	public void non_numeric_ISBNs_are_not_allowed() {
 		ValidateISBN validator = new ValidateISBN();
 		validator.checkISBN("helloworld");
 	}
